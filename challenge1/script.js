085f0c45d617716c3d99ae68e72dc4fb0ed75875
yavuzefe <yavuzefe@gmail.com>
2022-05-23 17:11:26 +0200

    finish 3 challanges

--- challenge1/script.js
+++ challenge1/script.js
@@ -12,5 +12,29 @@ box.addEventListener("focusout", function(){
 
 
 // Write your code here 👇
+box.addEventListener('keydown', (e) => {
+    let emoji;
 
+    switch (e.key) {
+        case "1":
+            emoji = "🤬";
+            break;
+        case "2":
+            emoji = "☹️";
+            break;
+        case "3":
+            emoji = "😐";
+            break;
+        case "4":
+            emoji = "🙂";
+            break;
+        case "5":
+            emoji = "😁";
+            break;
+        default:
+            emoji = "Type a number between 1 and 5";
+            break;
+    }
 
+    text.textContent = emoji;
+})
--- challenge2/script.js
+++ challenge2/script.js
@@ -8,4 +8,28 @@ solveButton.addEventListener("click", function() {
     solutionDisplay.innerHTML = ``;
     
     // Write your code here 👇
+    let equationValue = equationField.value;
+    let equationList = equationValue.split(" "); (3 + 5) 
+    let numb1 = parseInt(equationList[0]);
+    let numb2 = parseInt(equationList[2]);
+    let result;
+    switch (equationList[1]) {
+        case "+":
+            result = numb1 + numb2;
+            break;
+        case "-":
+            result = numb1 - numb2;
+            break;
+        case "*":
+            result = numb1 * numb2;
+            break;
+        case "/":
+            result = numb1 / numb2;
+            break;
+    }
+    let component = document.createElement('p');
+    component.classList.add('equation-component');
+    solutionDisplay.appendChild(component);
+    component.textContent = result;
 })
+
